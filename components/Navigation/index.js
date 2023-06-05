371007e26747b60822103972055e96b4186e1ca6
David Illies <dav.illies@web.de>
2023-06-05 10:44:34 +0200

    add navigation bar

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -3,20 +3,46 @@ import Link from "next/link";
 
 export default function Navigation() {
   return (
-    <ul>
-      <li>
-        <Link href="/">Spotlight</Link>
-      </li>
-      <li>
-        <Link href="/art-pieces">Art Pieces</Link>
-      </li>
-    </ul>
+    <StyledList>
+      <StyledListItem>
+        <StyledLink href="/">Spotlight</StyledLink>
+      </StyledListItem>
+      <StyledListItem>
+        <StyledLink href="/art-pieces">Art Pieces</StyledLink>
+      </StyledListItem>
+    </StyledList>
   );
 }
 
 const StyledList = styled.ul`
+  margin: 0;
+  padding: 0;
+
   height: 2rem;
+
+  position: fixed;
+  bottom: 0;
+  width: 100%;
 
   display: flex;
   justify-content: space-around;
+  align-items: center;
+
+  background-color: orange;
+`;
+
+const StyledListItem = styled.li`
+  list-style: none;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+
+  transition: 0.3s ease-in;
+
+  &:hover {
+    font-size: 1.1rem;
+    font-weight: bolder;
+  }
 `;
